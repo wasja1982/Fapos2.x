7eb272bb1ab01c2ad787011a1fdf73af070b1400
modos189 <modos189@ya.ru>
2013-01-03 18:34:06 +0000

    метка о изменении комментария

--- sys/inc/includes/_get_comments.php
+++ sys/inc/includes/_get_comments.php
@@ -11,11 +11,17 @@ $CommentsModel->bindModel('Users');
 if (empty($html)) {
 	/* pages nav */
 	$total = $CommentsModel->getTotal(array('cond' => array('entity_id' => $id)));
-	$this->_globalize(array('comments_pagination' => ''));
+	$per_page = $this->Register['Config']->read('comment_per_page', $this->module);
+    list($pages, $page) = pagination($total, $per_page,  '/users/user_posts/' . $id);
+	$this->_globalize(array('comments_pagination' => $pages));
 	
 	
 	$order_way = ($this->Register['Config']->read('comments_order', $this->module)) ? 'DESC' : 'ASC';
-	$params = array('order' => 'date ' . $order_way,);
+	$params = array(
+		'page'  => $page,
+		'limit' => $per_page,
+		'order' => 'date ' . $order_way,
+	);
 	$comments = $CommentsModel->getCollection(array('entity_id' => $id), $params);
 	if ($comments) {
 		foreach ($comments as $comment) {
@@ -66,11 +72,18 @@ if (empty($html)) {
 			
 			$markers['moder_panel'] = $moder_panel;
 			$markers['message'] = $this->Textarier->print_page($comment->getMessage());
+
+			if ($comment->getEditdate()!='0000-00-00 00:00:00') {
+				$markers['editdate'] = 'Комментарий был изменён '.$comment->getEditdate();
+			} else {
+				$markers['editdate'] = '';
+			}
+
 			$comment->setAdd_markers($markers);
 		}
 	}
 	$html = $this->render('viewcomment.html', array('commentsr' => $comments));
-
+	
 	
 } else {
 	$html = '';
