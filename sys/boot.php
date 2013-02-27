f815eb24f31839d18372602faabe41cc7c8c0685
wasja1982 <wasja@yandex.ru>
2013-02-27 22:47:28 +0300

    Фиксация версии форка за 27.02.2013.

--- sys/boot.php
+++ sys/boot.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['db_querys'])) unset($_SESSION['db_querys']);
 /**
  * Current version of engine
  */
-define('FPS_VERSION', '2.1 RC3 WE (25.02.13)');
+define('FPS_VERSION', '2.1 RC3 WE (27.02.13)');
 
 /**
  * Path constants
