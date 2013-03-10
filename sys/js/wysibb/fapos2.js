18688fd481f58ddd7f66b8a484db4da3063c927c
wasja1982 <wasja@yandex.ru>
2013-03-10 21:51:20 +0300

    Интегрирован WysiBB версии 1.4.1.

---
+++ sys/js/wysibb/fapos2.js
@@ -0,0 +1,83 @@
+WBBPRESET = {
+	buttons: 'bold,italic,underline,strike,|,justifyleft,justifycenter,justifyright,|,smilebox,|,code,quote,spoiler,hide,bullist,numlist,|,link,img,|,fontcolor,fontsize,removeFormat',
+	traceTextarea: true,
+	imgupload: false,
+	allButtons: {
+		spoiler : {
+			title: CURLANG.spoiler,
+			buttonText: 'spoiler',
+			transform : {
+				'<div><div><b>Сворачиваемый текст</b></div><div style="border: 1px inset ; overflow: auto;">{SELTEXT}</div></div>':"[spoiler]{SELTEXT}[/spoiler]"
+			}
+		},
+		hide : {
+			title: 'Скрытый текст',
+			buttonText: 'hide',
+			transform : {
+				'<div><div><b>Скрытый текст</b></div><div style="border: 1px inset ; overflow: auto;">{SELTEXT}</div></div>':"[hide]{SELTEXT}[/hide]"
+			}
+		},
+		quote : {
+			transform : { 
+				'<div class="bbQuoteBlock"><div class="bbQuoteName"><b>Цитата</b></div><div class="quoteMessage">{SELTEXT}</div></div>':'[quote]{SELTEXT}[/quote]',
+				'<div class="bbQuoteBlock"><div class="bbQuoteName"><b>{AUTHOR} пишет:</b></div><div class="quoteMessage">{SELTEXT}</div></div>':'[quote="{AUTHOR}"]{SELTEXT}[/quote]',
+				'<div style="" class="bbQuoteBlock"><div class="bbQuoteName"><b>{AUTHOR} пишет:</b></div><div class="quoteMessage">{SELTEXT}</div></div>':'[quote={AUTHOR}]{SELTEXT}[/quote]'
+			}
+		},
+		code: {
+			transform: {
+				'<div class="bbCodeBlock"><div class="bbCodeName"><b>Code:</b></div><div class="codeMessage" style="border: 1px inset ; overflow: auto;">{SELTEXT}</div></div>':'[code]{SELTEXT}[/code]',
+				'<div class="codePHP">{SELTEXT}</div>':'[php]{SELTEXT}[/php]',
+				'<div class="codeSQL">{SELTEXT}</div>':'[sql]{SELTEXT}[/sql]',
+				'<div class="codeJS">{SELTEXT}</div>':'[js]{SELTEXT}[/js]',
+				'<div class="codeCSS">{SELTEXT}</div>':'[css]{SELTEXT}[/css]',
+				'<div class="codeHTML">{SELTEXT}</div>':'[html]{SELTEXT}[/html]',
+				'<div class="codeHTML codeXML">{SELTEXT}</div>':'[xml]{SELTEXT}[/xml]'
+			}
+		},
+		bullist: {
+			transform: {
+				'<ul>{SELTEXT}</ul>':'[list]{SELTEXT}[/list]',
+				'<li>{SELTEXT}</li>':'[*]{SELTEXT[^\[\]\*]}'
+			}
+		},
+		numlist: {
+			transform: {
+				'<ol>{SELTEXT}</ol>':'[list=1]{SELTEXT}[/list]',
+				'<ol type="a">{SELTEXT}</ol>':'[list=a]{SELTEXT}[/list]',
+				'<li>{SELTEXT}</li>':'[*]{SELTEXT[^\[\]\*]}'
+			}
+		},
+		img : {
+			transform : {
+				'<img src="{SRC}" />':"[imgl]{SRC}[/imgl]",
+				'<img style="float:left;" src="{SRC}" />':"[img]{SRC}[/img]"
+			}
+		},
+		fs_verysmall: {
+			transform: {
+				'<span style="font-size:50%;">{SELTEXT}</span>':'[size=50]{SELTEXT}[/size]'
+			}
+		},
+		fs_small: {
+			transform: {
+				'<span style="font-size:85%;">{SELTEXT}</span>':'[size=85]{SELTEXT}[/size]'
+			}
+		},
+		fs_normal: {
+			transform: {
+				'<span style="font-size:100%;">{SELTEXT}</span>':'[size=100]{SELTEXT}[/size]'
+			}
+		},
+		fs_big: {
+			transform: {
+				'<span style="font-size:150%;">{SELTEXT}</span>':'[size=150]{SELTEXT}[/size]'
+			}
+		},
+		fs_verybig: {
+			transform: {
+				'<span style="font-size:200%;">{SELTEXT}</span>':'[size=200]{SELTEXT}[/size]'
+			}
+		}
+	}
+}
