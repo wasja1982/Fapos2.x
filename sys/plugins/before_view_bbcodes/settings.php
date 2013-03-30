91970cb5996ab0c3b07ffe7e2132e88b6a01e525
wasja1982 <wasja@yandex.ru>
2013-03-30 19:52:06 +0300

    Плагин "BBCodes Editor" доработан под новую админ-панель.

--- sys/plugins/before_view_bbcodes/settings.php
+++ sys/plugins/before_view_bbcodes/settings.php
@@ -43,6 +43,7 @@ if (isset($_POST['ac'])) {
 		case 'edit':
 			$number = isset($_POST['number']) ? intval(trim($_POST['number'])) : null;
 			if (isset($number)) {
+				if (isset($editor_set[$number])) $editor['default'] = $editor_set[$number]['default'];
 				$editor_set[$number] = $editor;
 			} else {
 				$editor_set[] = $editor;
@@ -64,115 +65,103 @@ if (isset($_POST['ac'])) {
 	}
 }
 
-$output = '';
+$popups_content = '<div class="popup" id="addEditor" style="display: none;">
+	<div class="top">
+		<div class="title">Добавление редактора</div>
+		<div class="close" onclick="closePopup(\'addEditor\');"></div>
+	</div>
+	<form method="POST" action="">
+	<div class="items">
+		<div class="item">' . __('Title') . ':<br />
+			<input type="text" name="title" style="width:95%" />
+			<input type="hidden" name="ac" value="new" />
+		<div class="clear"></div></div>';
 
-$output .= '<table width="100%"><tr>
-	<td><input type="button" name="add" value="Добавить редактор" onClick="wiOpen(\'new\');" /></td>
-	<td align="right"><div align="right" class="topButtonL" id="cat_view"><input type="button" name="set" value="Выбрать редактор" onClick="wiOpen(\'set\');" /></div></td>
-	</tr></table>';
-
-$output .= '<div id="new_dWin" class="fps-win" style="position:absolute;top:100px;left:30%;width:40%;display:none">
-	<div class="xw-tl"><div class="xw-tr"><div class="xw-tc xw-tsps"></div>
-	</div></div><div class="xw-ml"><div class="xw-mr"><div align="center" class="xw-mc">
-	<form action="" method="POST">
-
-	<div class="form-item2">
-	' . __('Title') . ':<br />
-	<input type="text" name="title" style="width:95%" />
-	<input type="hidden" name="ac" value="new" />
-	<div style="clear:both;"></div></div>';
 
 foreach ($markers as $marker => $value) {
-	$output .= '<div class="form-item2">
+	$popups_content .= '<div class="item">
 		Маркер {{ ' . $marker . ' }} - 
 		' . $value . ':<br />';
-	$output .= ($marker == 'editor_text' || $marker == 'editor_forum_text' || $marker == 'editor_forum_quote' || $marker == 'editor_forum_name') ? 
+	$popups_content .= ($marker == 'editor_text' || $marker == 'editor_forum_text' || $marker == 'editor_forum_quote' || $marker == 'editor_forum_name') ? 
 		'<input type="text" name="' . $marker . '" style="width:95%" />' :
 		'<textarea name="' . $marker . '" cols="30" rows="3" style="width:95%" /></textarea>';
-	$output .= '<br /><div style="clear:both;"></div></div>';
+	$popups_content .= '<div class="clear"></div></div>';
 }
-
-$output .= '<div class="form-item2">
-	<input type="submit" name="send" value="' . __('Save') . '" />
-	<input type="button" onClick="hideWin(\'new\')" value="' . __('Cancel') . '" />
-	<div style="clear:both;"></div></div>
+	
+$popups_content .= '<div class="item submit">
+			<div class="left"></div>
+			<div style="float:left;" class="right">
+				<input type="submit" class="save-button" name="send" value="Сохранить">
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
 	</form>
-	</div></div></div><div class="xw-bl"><div class="xw-br"><div class="xw-bc">
-	<div class="xw-footer"></div></div></div></div>
-	</div>';
+</div>';
+
+
+$output = '
+	<div class="list">
+		<div class="title">Управление редакторами</div>
+		<div onclick="openPopup(\'addEditor\');" class="add-cat-butt"><div class="add"></div>Добавить редактор</div>';
 
 if ($editor_set && is_array($editor_set) && count($editor_set)) {
-	$output .= '<div id="set_dWin" class="fps-win" style="position:absolute;top:200px;left:40%;display:none">
-		<div class="xw-tl"><div class="xw-tr"><div class="xw-tc xw-tsps"></div>
-		</div></div><div class="xw-ml"><div class="xw-mr"><div align="center" class="xw-mc">
-		<form action="" method="POST">
-		<div class="form-item2">
-		Выберите редактор:<br />';
-
-	$output .= '<select style="width:95%;" name="number">';
-	foreach ($editor_set as $index => $editor) {
-		$output .= '<option value="' . $index . '"' . ((isset($editor['default']) && $editor['default']) ? 'selected="selected"' : '') . '>' . h($editor['title']) . '</option>';
-	}
-	$output .= '</select>';
-
-	$output .= '<input type="hidden" name="ac" value="set" />
-		<div style="clear:both;"></div></div>';
-
-	$output .= '<div class="form-item2">
-		<input type="submit" name="send" value="' . __('Save') . '" />
-		<input type="button" onClick="hideWin(\'set\')" value="' . __('Cancel') . '" />
-		<div style="clear:both;"></div></div>
-		</form>
-		</div></div></div><div class="xw-bl"><div class="xw-br"><div class="xw-bc">
-		<div class="xw-footer"></div></div></div></div>
-		</div>';
-
-
-	$output .= '<div class="cat_list_container">';
-	foreach ($editor_set as $index => $editor) {
-		$output .= '
-			<div class="category_row">
-			<b>' . (isset($editor['title']) ? $editor['title'] : 'Редактор ' . $index) . '</b>
-			<div class="tools">
-			<form action="" method="POST">
-			<a href="javascript://" onClick="wiOpen(\'' . $index . '_editor\')"><img src="template/img/edit_16x16.png"  /></a>
+	$output .= '
+		<form name="deleteEditor" action="" method="POST">
 			<input type="hidden" name="ac" value="del" />
-			<input type="hidden" name="number" value="' . $index . '" />
-			<input type="image" src="template/img/del.png" onClick="return _confirm();" />
-			</form>';
-		$output .= '		
-			<div id="' . $index . '_editor_dWin" class="fps-win" style="position:absolute;top:100px;left:30%;width:40%;display:none">
-			<div class="xw-tl"><div class="xw-tr"><div class="xw-tc xw-tsps"></div>
-			</div></div><div class="xw-ml"><div class="xw-mr"><div align="center" class="xw-mc">
-			<form action="" method="POST">
-
-			<div class="form-item2">
-			' . __('Title') . ':<br />
+			<input type="hidden" name="number" value="" />
+		</form>
+		<form name="setEditor" action="" method="POST">
+			<input type="hidden" name="ac" value="set" />
+			<input type="hidden" name="number" value="" />
+		</form>
+		<div class="level1">
+			<div class="items">';
+	foreach ($editor_set as $index => $editor) {
+		$output .= '<div class="level2">
+					<div class="title">' . (isset($editor['title']) && strlen($editor['title']) > 0 ? h($editor['title']) : 'Редактор ' . $index) . '</div>
+					<div class="buttons">
+						<a class="edit" onclick="openPopup(\'editEditor' . $index . '\')" href="javascript://"></a>
+						<a class="' . ((isset($editor['default']) && $editor['default']) ? 'on' : 'off') . '" onclick="document.forms[\'setEditor\'].number.value=' . $index . ';document.forms[\'setEditor\'].submit();" href="javascript://"></a>
+						<a class="delete" onclick="if (_confirm()) {document.forms[\'deleteEditor\'].number.value=' . $index . ';document.forms[\'deleteEditor\'].submit();};" href="javascript://"></a>
+					</div>
+				</div>';
+		$popups_content .= '<div class="popup" id="editEditor' . $index . '">
+	<div class="top">
+		<div class="title">Настройка редактора</div>
+		<div class="close" onclick="closePopup(\'editEditor' . $index . '\');"></div>
+	</div>
+	<form method="POST" action="">
+	<div class="items">
+		<div class="item">' . __('Title') . ':<br />
 			<input type="text" name="title" value="' . (isset($editor['title']) ? htmlspecialchars($editor['title']) : '') . '" style="width:95%" />
 			<input type="hidden" name="ac" value="edit" />
 			<input type="hidden" name="number" value="' . $index . '" />
-			<div style="clear:both;"></div></div>';
+		<div class="clear"></div></div>';
 
 		foreach ($markers as $marker => $value) {
-			$output .= '<div class="form-item2">
+			$popups_content .= '<div class="item">
 				Маркер {{ ' . $marker . ' }} - 
 				' . $value . ':<br />';
-			$output .= ($marker == 'editor_text' || $marker == 'editor_forum_text' || $marker == 'editor_forum_quote' || $marker == 'editor_forum_name') ? 
+			$popups_content .= ($marker == 'editor_text' || $marker == 'editor_forum_text' || $marker == 'editor_forum_quote' || $marker == 'editor_forum_name') ? 
 				'<input type="text" name="' . $marker . '" style="width:95%" value="' . (isset($editor[$marker]) ? htmlspecialchars($editor[$marker]) : '') . '" />' :
 				'<textarea name="' . $marker . '" cols="30" rows="3" style="width:95%" />' . (isset($editor[$marker]) ? htmlspecialchars($editor[$marker]) : '') . '</textarea>';
-			$output .= '<br /><div style="clear:both;"></div></div>';
+			$popups_content .= '<div class="clear"></div></div>';
 		}
-
-		$output .= '<div class="form-item2 center">
-			<input type="submit" name="send" value="' . __('Save') . '" />
-			<input type="button" onClick="hideWin(\'' . $index . '_editor\')" value="' . __('Cancel') . '" />
-			<div style="clear:both;"></div></div>
-			</form>
-			</div></div></div><div class="xw-bl"><div class="xw-br"><div class="xw-bc">
-			<div class="xw-footer"></div></div></div></div>
+	
+		$popups_content .= '<div class="item submit">
+			<div class="left"></div>
+			<div style="float:left;" class="right">
+				<input type="submit" class="save-button" name="send" value="Сохранить">
 			</div>
-			</div></div>';
+			<div class="clear"></div>
+		</div>
+	</div>
+	</form>
+</div>';
 	}
-	$output .= '</div>';
 }
+$output .= '</div></div>';
+$output = $popups_content . $output;
+
 ?>
